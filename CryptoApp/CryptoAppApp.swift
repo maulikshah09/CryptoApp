c12e615bcf820e021c504bea396eff617d244462
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-17 12:47:10 +0530

    Maulik -> Crypto App

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Maulik Shah on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
